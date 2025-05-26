dcf9427bb0e95d8571b0e10b3eea1fd0dcfbed5f
Mlsoun <53565012+Mlsoun@users.noreply.github.com>
2025-05-26 15:54:30 +0200

    Add project files.

---
+++ MVC/Controllers/SocksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
+
+namespace MVC.Controllers
+{
+    [Route("ponozky")]
+    [Route("socks")]
+    public class SocksController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = SocksDataset.GetSocks();
+
+            return View(data);
+        }
+
+        [Route("[action]/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = SocksDataset.GetSocks().Where(x => x.Id == id).FirstOrDefault();
+
+            return View(data);
+           
+        }
+
+        //[Route("[action]/min/{priceMin:int}/max/{priceMax:int}")]
+        //public IActionResult searchByPrice(int priceMin, int priceMax)
+        //{
+
+        //}
+    }
+}
---
+++ MVC/Data/SocksDataset.cs
@@ -0,0 +1,22 @@
+﻿using MVC.Models;
+
+namespace MVC.Data
+{
+    public class SocksDataset
+    {
+        static string[] brands = ["icebreaker", "addidas", "rebook", "nike"];
+        public static IEnumerable<Socks> GetSocks()
+        {
+            return Enumerable.Range(1, 5).Select(index =>
+                new Socks()
+                {
+                    Id = index,
+                    Brand = brands[Random.Shared.Next(brands.Length)],
+                    Size = (SockSize)Random.Shared.Next(4),
+                    Price = Random.Shared.Next(50, 500),
+                    InStock = Random.Shared.Next(0, 20)
+                }).ToArray();
+
+        }
+    }
+}
